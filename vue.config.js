6637758b8c6e22aba943455d1c9bbd8d277df0aa
WeiFeng <46436708+WeiFengWa@users.noreply.github.com>
2022-05-13 21:13:55 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  /*devServer: {
+    host: 'localhost',  // 将浏览地址的本地服务修改为本机ip地址，用来到手机上测试
+    port: 8080,           //端口号
+  },*/
+})
